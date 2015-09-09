8266fbf8cb98be131095f4e6d445a983dbc38fed
jpkuo <sleepminded@gmail.com>
2015-09-09 16:32:31 -0700

    shittymhdmdcalc
    Python MH Damage Calc

---
+++ shittymhdmgcalc.py
@@ -0,0 +1,39 @@
+#Marvel Heroes Calculator
+import io
+import math
+import tkinter as tk
+
+root = tk.Tk()
+
+#labels
+label0 = tk.Label( root, text="Enter all values as seen on character info" )
+label1 = tk.Label( root, text="Base Crit rating: " )
+label2 = tk.Label( root, text="Base Melee Crit rating: " )
+
+label0.grid(column = 0, row = 0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+#entries
+
+
+
+
+
+
+
+
+
+
+
+#buttons
