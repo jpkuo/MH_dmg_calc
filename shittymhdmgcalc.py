213dd13fdc4f4e1dd5701615934fab1fefdd8392
jpkuo <sleepminded@gmail.com>
2015-09-09 17:11:00 -0700

    Organizing
    Setting up looks

--- shittymhdmgcalc.py
+++ shittymhdmgcalc.py
@@ -4,18 +4,40 @@ import math
 from tkinter import *
 import tkinter as tk
 
-root = Tk()
+#root = Tk()
+main = PanedWindow()
+main.pack(fill = BOTH, expand = 1)
+sec = PanedWindow(main, orient = VERTICAL )
 
-group1 = LabelFrame( root, text="Character Ratings", padx = 5, pady = 5 )
+group1 = LabelFrame( main, text="Character Ratings", padx = 5, pady = 5 )
 group1.pack( padx = 10, pady = 10 )
+
+group2 = LabelFrame( main, text="Character Attributes", padx = 5, pady = 5 )
+group2.pack( padx = 10, pady = 10 )
+
+group3 = LabelFrame( main, text="Synergies", padx = 5, pady = 5 )
+group3.pack( padx = 10, pady = 10 )
+
+group4 = LabelFrame( main, text="Extra Bonuses", padx = 5, pady = 5 )
+group4.pack( padx = 10, pady = 10 )
+
+main.add(group1)
+main.add(group2)
+main.add(sec)
+sec.add(group3)
+sec.add(group4)
 
 group1toplabel = Label( group1, text="Enter Values as seen on character info" )
 group1toplabel.pack()
 
-group2 = LabelFrame( root, text="Character Attributes", padx = 5, pady = 5 )
-group2.pack( padx = 10, pady = 10 )
-
 group2toplabel = Label( group2, text="Enter your Character's Attributes" )
+group2toplabel.pack()
+
+group3toplabel = Label( group3, text="Please pick only 10" )
+group3toplabel.pack()
+
+group4toplabel = Label( group4, text="Triggered Bonuses" )
+group4toplabel.pack()
 
 mainloop()
 
